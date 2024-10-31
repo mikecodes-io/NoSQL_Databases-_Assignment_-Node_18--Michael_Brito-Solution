796ae94700a5cec4a0baf401331a78ad8020baa2
mikecodes.io <1michaelbrito@gmail.com>
2024-10-30 22:49:18 -0400

    update 1

--- knexfile.js
+++ knexfile.js
@@ -1,47 +1,11 @@
-// Update with your config settings.
+require("dotenv").config();
+const { DATABASE_URL } = process.env;
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
 
   development: {
     client: 'postgresql',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    connection: DATABASE_URL
+    
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
 };
